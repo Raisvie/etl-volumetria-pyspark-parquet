6958e22e99b2a37ca036e3654150ba4bb908d082
Raivie <raissavieirasouza98@gmail.com>
2025-07-14 11:26:46 -0300

    feat(data-gen): adiciona script para geração de dados sintéticos de pedidos com 1 milhão de registros

--- Dado_sinteticos_SOR.py
+++ Dado_sinteticos_SOR.py
@@ -4,16 +4,21 @@ import pandas as pd
 import random
 
 fake = Faker()
-data = []
-for _ in range(1_000_000):
-    data.append({
-        "pedido_id": fake.uuid4(),
-        "restaurante": fake.company(),
-        "cliente_id": fake.uuid4(),
+#padronização de dados para testes futuros
+#random.seed(42)
+
+data_gerados = []
+
+## geração de 1 milhão de registros sintéticos de pedidos para simulação
+for i in range(1_000_000):
+    data_gerados.append({
+        "pedido_id": f"{random.randint(10000, 99999)}",
+        "restaurante_id": f"{random.randint(10000, 99999)}",
+        "cliente_id": f"{random.randint(10000, 99999)}",
         "valor_total": round(random.uniform(10, 150), 2),
         "data_pedido": fake.date_between(start_date='-30d', end_date='today').isoformat(),
         "forma_pagamento": random.choice(['crédito', 'débito', 'pix', 'voucher']),
     })
 
-df = pd.DataFrame(data)
-df.to_csv("data/raw/pedidos.csv", index=False)
+df_SOR = pd.DataFrame(data_gerados)
+df_SOR.to_parquet("pedidos.parquet", index=False)
